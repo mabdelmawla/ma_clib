1686979c3301b6bb11baa2966677313a18da25c5
Mohamed Abdelmawla <mohamed.abdelmawla@gmail.com>
2018-11-11 19:56:59 +0100

    Added license agreement to the files

--- inc/ma_common.h
+++ inc/ma_common.h
@@ -4,6 +4,19 @@
 @author		M. Abdelmawla
 @date		22 DEC 2017
 @version	1.0
+
+ma_clib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ma_clib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ma_clib.  If not, see <https://www.gnu.org/licenses/>.
 */
 #ifndef _MA_COMMON_H_
 #define _MA_COMMON_H_
--- src/aes/ma_aes_common.c
+++ src/aes/ma_aes_common.c
@@ -4,6 +4,19 @@
 @author		M. Abdelmawla
 @date		23 DEC 2017
 @version	1.0
+
+ma_clib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ma_clib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ma_clib.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "ma_aes_common.h"
