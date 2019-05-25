045ac2351a9951b1af7532dd2b7540c2dff793be
mabdelmawla <mohamed.abdelmawla@gmail.com>
2019-05-25 19:05:15 +0200

    generalized the test

--- tst/tst_ma_aes.c
+++ tst/tst_ma_aes.c
@@ -9,6 +9,36 @@
 #include "tst_ma_aes.h"
 #include <ma_aes.h>
 
+typedef struct _t_str_aes_input_ {
+	_t_ma_u8 plain_txt[16];
+	_t_ma_u8 key[16];
+	_t_ma_u8 cipher_txt[16];
+} _t_str_aes_input;
+
+static _t_str_aes_input gstr_aes_input[] = {
+		{
+			{
+				0x32, 0x43, 0xf6, 0xa8,
+				0x88, 0x5a, 0x30, 0x8d,
+				0x31, 0x31, 0x98, 0xa2,
+				0xe0, 0x37, 0x07, 0x34
+
+			},
+			{
+				0x2b, 0x7e ,0x15 ,0x16,
+				0x28 ,0xae ,0xd2 ,0xa6,
+				0xab ,0xf7 ,0x15 ,0x88,
+				0x09 ,0xcf ,0x4f ,0x3c
+			},
+			{
+				0x39, 0x02, 0xdc, 0x19,
+				0x25, 0xdc, 0x11, 0x6a,
+				0x84, 0x09, 0x85, 0x0b,
+				0x1d, 0xfb, 0x97, 0x32
+			}
+		},
+};
+
 _t_ma_err tst_ma_aes_ecb_encrypt(char *fn_name){
 	_t_ma_err ret = MA_ERR_OK;
 	int i;
@@ -37,10 +67,16 @@ Cipher Key = 2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c
 	};
 	_t_ma_u8 out[16];
 
-	ma_aes_ecb_encrypt(inp,out,key,MA_AES_NK_128);
+	//ma_aes_ecb_encrypt(inp,out,key,MA_AES_NK_128);
+	ma_aes_ecb_encrypt(
+			gstr_aes_input[0].plain_txt,
+			out,
+			gstr_aes_input[0].key,
+			MA_AES_NK_128
+			);
 
 	for(i = 0; i < 16; i++){
-		if(ref[i] != out[i]){
+		if(gstr_aes_input[0].cipher_txt[i] != out[i]){
 			ret = MA_ERR_NOT_OK;
 		}
 	}
