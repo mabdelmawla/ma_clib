58fbd0917967764fb432b08d2a0cf9c937ffbed5
mabdelmawla <mohamed.abdelmawla@gmail.com>
2020-01-11 22:07:36 +0100

    staged unstaged files

---
+++ src/ma_sha/ma_sha.h
@@ -0,0 +1,27 @@
+/**
+@file		ma_sha.h
+@brief		This module contains SHA operation APIs
+@author		M. Abdelmawla
+@date		11 JAN 2020
+@version	1.0
+
+ma_clib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ma_clib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ma_clib.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef SRC_MA_SHA_MA_SHA_H_
+#define SRC_MA_SHA_MA_SHA_H_
+
+
+
+#endif /* SRC_MA_SHA_MA_SHA_H_ */
---
+++ src/ma_sha/ma_sha_common.h
@@ -0,0 +1,35 @@
+/**
+@file		ma_sha_common.h
+@brief		This module contains SHA Common operation APIs
+@author		M. Abdelmawla
+@date		11 JAN 2020
+@version	1.0
+
+ma_clib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ma_clib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ma_clib.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef SRC_MA_SHA_MA_SHA_COMMON_H_
+#define SRC_MA_SHA_MA_SHA_COMMON_H_
+
+#include <ma_common.h>
+
+/**
+Defines
+*/
+/**
+Typedefs
+*/
+
+
+#endif /* SRC_MA_SHA_MA_SHA_COMMON_H_ */
